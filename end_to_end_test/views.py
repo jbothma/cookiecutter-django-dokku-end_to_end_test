e388f90ee1ba13b6b0918475eabc46491e549e2b
JD Bothma <jbothma@gmail.com>
2020-01-26 00:39:52 +0200

    Remove the added code again

--- end_to_end_test/views.py
+++ end_to_end_test/views.py
@@ -3,8 +3,3 @@ from django.views import generic
 
 class Index(generic.TemplateView):
     template_name = "index.html"
-
-    def get_context_data(self, **kwargs):
-        print("here")
-        context = super().get_context_data(**kwargs)
-        return context
